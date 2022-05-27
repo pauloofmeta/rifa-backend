988314f14a1d8d9ea8faa73d7ffc0a784cbe7374
Paulo Henrique Sousa da Silva <paulo.henrique@carguero.com.br>
2022-05-26 23:25:34 -0300

    Ajuste

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,7 +1,11 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 
 const app = initializeApp({
-  credential: cert('src/config/serviceAccount.json')
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')    
+  })
 });
 
 export default app;
